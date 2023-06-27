37c900a342529e160cd4dd840b244f45bc316298
Tejal Salve <60121669+tejalsalve19@users.noreply.github.com>
2023-06-28 00:11:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function applyPreferences() {
+  var fontSize = document.getElementById("fontsize").value;
+  var fontColor = document.getElementById("fontcolor").value;
+
+  document.body.style.fontSize = fontSize + "px";
+  document.body.style.color = fontColor;
+
+  // Store user preferences in cookies
+  document.cookie = "fontSize=" + fontSize + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+  document.cookie = "fontColor=" + fontColor + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+}
+
+// Function to retrieve user preferences from cookies
+function retrievePreferences() {
+  var cookies = document.cookie.split(";");
+
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i].trim();
+
+    if (cookie.indexOf("fontSize=") === 0) {
+      var fontSize = cookie.substring("fontSize=".length);
+      document.getElementById("fontsize").value = fontSize;
+      document.body.style.fontSize = fontSize + "px";
+    } else if (cookie.indexOf("fontColor=") === 0) {
+      var fontColor = cookie.substring("fontColor=".length);
+      document.getElementById("fontcolor").value = fontColor;
+      document.body.style.color = fontColor;
+    }
+  }
+}
